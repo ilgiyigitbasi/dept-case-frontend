4be934518dbd85e12b8bb92b88762202601b7759
ilgiyigitbasi <ilgiyigitbasi@gmail.com>
2022-08-27 16:48:34 +0300

    footer logo fix and scroll to top feature added

--- components/footer.jsx
+++ components/footer.jsx
@@ -7,29 +7,36 @@ const Footer = () => {
     const socialMediaIcons = ['/facebook.png',  '/twitter.png','/instagram.png',]
     return (
         <>
-            <div className={Styles.mainContainer}>
-            <div className={Styles.firstRow}>
-               <div className={Styles.menu}> <Image className={Styles.logo} src="/DEPTWhite.png" alt="dept" height={25} width={88.04}/>
-                   <div className={Styles.menuItems}> {menuItems.map((name)=> <span className={Styles.item} key={name}>{name.toUpperCase()}</span>)}</div>
+            <div className={Styles.footerContainer}>
+                <div className={Styles.mainContainer}>
+                    <div className={Styles.firstRow}>
+                        <div className={Styles.menu}> <img className={Styles.logo} src="/DEPTWhite.png" alt="dept" height={25} width={88.04}/>
+                            <div className={Styles.menuItems}> {menuItems.map((name)=> <span className={Styles.item} key={name}>{name.toUpperCase()}</span>)}</div>
 
-               </div>
+                        </div>
 
-                <div className={Styles.socialMediaIcons}>
-                    {socialMediaIcons.map((icon)=> <img alt="" style={{marginLeft: '10px'}} key={icon} src={icon} height={15}/>)}
-                </div>
+                        <div className={Styles.socialMediaIcons}>
+                            {socialMediaIcons.map((icon)=> <img alt="" style={{marginLeft: '10px'}} key={icon} src={icon} height={15}/>)}
+                        </div>
 
 
-            </div>
-                <div className={Styles.secondRow}>
-                    <div>
-                        <span>Chamber of Commerce: 63464101  </span>
-                        <span>VAT: NL 8552.47.502.B01 </span>
-                        <span>Terms and conditions  </span>
                     </div>
+                    <div className={Styles.secondRow}>
+                        <div>
+                            <span>Chamber of Commerce: 63464101  </span>
+                            <span>VAT: NL 8552.47.502.B01 </span>
+                            <span>Terms and conditions  </span>
+                        </div>
 
-                    <div style={{textAlign:'end'}}>© 2022 Dept Agency</div>
+                        <div style={{textAlign:'end'}}>© 2022 Dept Agency</div>
+                    </div>
+                </div>
+                <div onClick={()=> window.scrollTo(0,0)}>
+                    <Image alt={'arrow'} src={'/arrowUp.png'} height={30} width={15} />
+                    <div>TOP</div>
                 </div>
             </div>
+
         </>
     );
 };
